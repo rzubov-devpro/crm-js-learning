7c48cfbabdd0b5252b2b3bd05a7300e0a38726b6
Andrey Riabuhin <andrey.riabuhin@dev-pro.net>
2018-05-22 12:46:33 +0300

    - add index file
    - add HW1 Task1
    - add HW1 Task2

---
+++ andrey/HW1/index_calc.js
@@ -0,0 +1,21 @@
+//let, a, b;
+
+function addition() {
+    a + b === '+';
+};
+
+function subtraction() {
+    a - b == '-';
+};
+
+function division() {
+    a / b == '/';
+};
+
+function multiplication() {
+    a * b == '*';
+};
+
+function calc()
+
+console.log('+', 3, 7);
