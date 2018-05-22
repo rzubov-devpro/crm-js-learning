045ab64c2657101653416b33de234b89c3929f07
Ihor Kremenskiy <ihor.kremenskiy@dev-pro.net>
2018-05-22 12:30:07 +0300

    HW 1 is done
    HW 2 is done
    HW 3 is done

---
+++ HW2/index.js
@@ -0,0 +1,18 @@
+// Игорь
+// Create a function isDivisible. isDivisible(n, x, y) should checks if a number n is divisible by two numbers x AND y.
+
+// Example:
+// isDivisible(3,1,3)--> true because 3 is divisible by 1 and 3
+// isDivisible(12,2,6)--> true because 12 is divisible by 2 and
+
+function isDivisible(n, x, y) {
+
+    if (n % x === 0 && n % y === 0) {
+        return ('YES, this number is divisble!')
+    }
+    return ('NO, this number is NOT divisble')
+}
+
+console.log('Do you think 3 is divisible by 1 and 3? ' + '\n' + '\n' + isDivisible(3,1,3))
+console.log('What is about 28 is divisible by 7 and 9? ' + '\n' + '\n' + isDivisible(28,7,9))
+
