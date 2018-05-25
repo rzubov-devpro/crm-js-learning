16c5ecbc2d7bc9c2b55998971f77361dc6fd9b8b
Andrey Riabuhin <andrey.riabuhin@dev-pro.net>
2018-05-25 13:42:00 +0300

    Enter commit message

---
+++ andrey/HW1/Task1.js
@@ -0,0 +1,38 @@
+const X = 0;
+
+function addPositive(a, b, c, d) {
+    if (a < 0) {
+        a = X;
+    } if (b < 0) {
+        b = X;
+    } if (c < 0) {
+        c = X;
+    } if (d < 0) {
+        d = X;
+    }
+    return a + b + c + d;
+};
+
+
+
+function includeNegative(a, b, c, d) {
+    return a + b + c + d;
+};
+
+function addFunction(a, b, c, d) {
+    if (confirm('Ignore negatives?')) {
+        return addPositive(a, b, c, d);
+    } else {
+        return includeNegative(a, b, c, d);
+    }
+};
+
+console.log(addFunction(1, -4, 7, 12));
+
+
+
+
+
+
+
+
---
+++ andrey/HW1/Task2.js
@@ -0,0 +1,18 @@
+let a, b, mathOp;
+
+function calculator(mathOp, a, b) {
+   if (mathOp == '+') {
+       return a + b;
+    }
+    if (mathOp == '-') {
+        return a - b;
+    }
+    if (mathOp == '/') {
+        return a / b;
+    }
+    if (mathOp == '*') {
+        return a * b;
+    }
+};
+
+console.log(calculator('+',3, 7));
